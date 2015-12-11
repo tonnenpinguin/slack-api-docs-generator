b7655837a1dacafe3e96ce0313824e1136462c24
aki017 <aki@aki017.info>
2015-12-11 15:48:07 +0900

    Add travis integration

---
+++ ci.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+D_DIR=$(mktemp -d)
+echo "$D_DIR"
+rm -rf "$D_DIR"
+git clone git@github.com:aki017/slack-api-docs "$D_DIR" -b auto
+bundle exec ruby main.rb "$D_DIR"
+cd "$D_DIR" && git add . && git commit -m "auto commited `date`" && git push origin auto
+rm -rf "$D_DIR"
