c0522c608a816d118242274e5464598809ad4947
aki017 <aki@aki017.info>
2016-08-10 18:38:34 +0900

    Set auto branch to master [aki017/slack-api-docs#2]

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 D_DIR=$(mktemp -d)
 echo "$D_DIR"
 rm -rf "$D_DIR"
-git clone git@github.com:aki017/slack-api-docs "$D_DIR" -b auto
+git clone git@github.com:aki017/slack-api-docs "$D_DIR" -b master
 bundle exec ruby main.rb "$D_DIR"
-cd "$D_DIR" && git add . && git commit -m "auto commited `date`" && git push origin auto
+cd "$D_DIR" && git add . && git commit -m "auto commited `date`" && git push origin master
 rm -rf "$D_DIR"
